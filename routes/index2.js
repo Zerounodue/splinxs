4b3087d60d61eafddfa152800cc0728c7fca5729
luthp3 <luthp3@bfh.ch>
2016-04-12 13:59:10 +0200

    in db  search for guide and check if geo location is in area

--- routes/index2.js
+++ routes/index2.js
@@ -4,6 +4,7 @@ var router = express.Router();
 //mongodb stuff
 var passport = require('passport');
 var Account = require('../models/account');
+var Guide = require('../models/guide');
 
 //https://github.com/meikidd/iso-639-1
 var ISO6391 = require('iso-639-1');
@@ -150,7 +151,48 @@ router.post('/knownAreas', function(req, res) {
 
 });
 
+router.get('/db', function(req, res) {
+    /*
+    Guide.register(new Guide({ username : 'Mike' }), '123', function(err, guide) {
+        if (err) {
+            console.log('authentication failed');
+        }
 
+        passport.authenticate('local')(req, res, function () {
+            
+            console.log('okee');
+        });
+    });
+    */
+    var id = "570cddce83743d7c114a7e42";
+    //Guide.update({ _id: id }, { $set: { languages: ['en', 'de'], areas: [{lat: 46.947248, lng: 7.451586, radius: 100}] }}, callback);
+    
+    //Guide.update({ _id: id }, { $set: { areas: [{lat: 46.947248, lng: 46.947248, radius: 100}] } }, callback);
+    
+    //Guide.findByIdAndRemove({ _id: id }, callback);
+    
+    
+    Guide.findOne({ '_id': id }, 'areas', function (err, guide) {
+        //error occured
+        if (err) return handleError(err);
+        var a = guide.isInArea({lat: 46.947248, lng: 7.451586});
+        console.log('-------------is in area: ' + a);
+        
+        a = guide.isInArea({lat: 55.947248, lng: 15.451586});
+        console.log('-------------is in area: ' + a);
+    });
+    
+    
+    
+    function callback(){
+        Guide.find(function(err, guides){
+            console.log(guides);
+            res.send('<a>' + guides + '</a>');
+        });
+    }
+
+    //res.render('knownAreas', {dbAreas: areas});
+});
 
 
 
